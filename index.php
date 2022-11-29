ebb7dced7d4fa9dcacd550cf00160db0bea309fd
Sheveleva Nadegda <sheveleva-n02@mail.ru>
2022-11-30 01:34:55 +0300

    Try to use function

--- index.php
+++ index.php
@@ -53,11 +53,39 @@ function callback_handleEvent()
                 $get_params = http_build_query($request_params);
 
                 file_get_contents('https://api.vk.com/method/messages.send?' . $get_params);
-                break;
+
+                echo('ok');
+                exit();
+//                break;
 
             default:
-                _callback_response('Unsupported event');
-                break;
+                $token = 'vk1.a.9j7qT1t6Q0Ck1sd74dyc38cboQacFV8WOIwlfC8PO-nzxcPZ6-8KArJ8P6A5Vfy5VVT_FzgVvr7ZQpovh2GvFdTMyDgtQ4W_XqI0Vdpg7LUdgw0WBg3fSSuBQq8SA5w22MNRMFZEt9YwsHelV0NLa_6RujYLHZeImP9fP8sPJZrrcZUy_MvPjoFRTEmWLuKi-xZbsW9AvROhiiOyEzLM2w';
+
+//                _callback_handleMessageNew($event['object']);
+                $user_id = $event->object->message->from_id;
+//затем с помощью users.get получаем данные об авторе
+                $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={$token}&v=5.103"));
+
+//и извлекаем из ответа его имя
+                $user_name = $user_info->response[0]->first_name;
+
+//С помощью messages.send отправляем ответное сообщение
+                $request_params = array(
+                    'message' => "What, {$user_name}!",
+                    'peer_id' => $user_id,
+                    'access_token' => $token,
+                    'v' => '5.103',
+                    'random_id' => '0'
+                );
+
+                $get_params = http_build_query($request_params);
+
+                file_get_contents('https://api.vk.com/method/messages.send?' . $get_params);
+
+                echo('ok');
+                exit();
+//                _callback_response('Unsupported event');
+//                break;
         }
     } catch (Exception $e) {
         log_error($e);
