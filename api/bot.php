41488b148620384e30bf4b5e8cc9d8187b2aa616
Sheveleva Nadegda <sheveleva-n02@mail.ru>
2022-11-30 02:38:40 +0300

    Change structure in code /bot

--- api/bot.php
+++ api/bot.php
@@ -3,7 +3,7 @@
 function bot_sendMessage($user_id)
 {
     $users_get_response = vkApi_usersGet($user_id);
-    $user = array_pop($users_get_response);
+    $user = $users_get_response->response[0];
     $msg = "Привет, {$user->first_name}!";
 
     vkApi_messagesSend($user_id, $msg);
