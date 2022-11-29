da2a8acda9b27e555f6e2c6c36464c24614fa3b6
Sheveleva Nadegda <sheveleva-n02@mail.ru>
2022-11-30 02:29:18 +0300

    Change structure in code /bot

--- api/bot.php
+++ api/bot.php
@@ -2,21 +2,21 @@
 
 function bot_sendMessage($user_id)
 {
-//    $users_get_response = vkApi_usersGet($user_id);
-//    $user = array_pop($users_get_response);
-//    $msg = "Привет, {$user['first_name']}!";
+    $users_get_response = vkApi_usersGet($user_id);
+    $user = array_pop($users_get_response);
+    $msg = "Привет, {$user['first_name']}!";
 //
 //    vkApi_messagesSend($user_id, $msg);
 
     $token = VK_API_ACCESS_TOKEN;
-    $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={$token}&v=5.103"));
+//    $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={$token}&v=5.103"));
 
 //и извлекаем из ответа его имя
-    $user_name = $user_info->response[0]->first_name;
+//    $user_name = $user_info->response[0]->first_name;
 
 //С помощью messages.send отправляем ответное сообщение
     $request_params = array(
-        'message' => "Hello, {$user_name}!",
+        'message' => "Hello, {$user}!",
         'peer_id' => $user_id,
         'access_token' => $token,
         'v' => '5.103',
