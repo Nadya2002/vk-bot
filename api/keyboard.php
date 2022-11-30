e5aba75e179d9635c79211068648ed6888c1d96c
Sheveleva Nadegda <sheveleva-n02@mail.ru>
2022-12-01 01:44:37 +0300

    Debug keyboard

--- api/keyboard.php
+++ api/keyboard.php
@@ -60,86 +60,86 @@ function keyboard_choose_group()
 
 function keyboard_choose_day()
 {
-//    $keyboard = [
-//        "one_time" => false,
-//        "buttons" => [
-//            [["action" => [
-//                "type" => "text",
-//                "payload" => '{"button": "mon"}',
-//                "label" => "Понедельник"],
-//                "color" => "default"]],
-//            [["action" => [
-//                "type" => "text",
-//                "payload" => '{"button": "tue"}',
-//                "label" => "Вторник"],
-//                "color" => "default"]],
-//            [["action" => [
-//                "type" => "text",
-//                "payload" => '{"button": "wed"}',
-//                "label" => "Среда"],
-//                "color" => "default"]],
-//            [["action" => [
-//                "type" => "text",
-//                "payload" => '{"button": "thu"}',
-//                "label" => "Четверг"],
-//                "color" => "default"]],
-//            [["action" => [
-//                "type" => "text",
-//                "payload" => '{"button": "fri"}',
-//                "label" => "Пятница"],
-//                "color" => "default"]],
-//            [["action" => [
-//                "type" => "text",
-//                "payload" => '{"button": "sat"}',
-//                "label" => "Суббота"],
-//                "color" => "default"]],
-//            [["action" => [
-//                "type" => "text",
-//                "payload" => '{"button": "sun"}',
-//                "label" => "Воскресенье"],
-//                "color" => "default"]]
-//        ]
-//    ];
-//    return $keyboard;
+    $keyboard = [
+        "one_time" => false,
+        "buttons" => [
+            [["action" => [
+                "type" => "text",
+                "payload" => '{"button": "mon"}',
+                "label" => "Понедельник"],
+                "color" => "default"]],
+            [["action" => [
+                "type" => "text",
+                "payload" => '{"button": "tue"}',
+                "label" => "Вторник"],
+                "color" => "default"]],
+            [["action" => [
+                "type" => "text",
+                "payload" => '{"button": "wed"}',
+                "label" => "Среда"],
+                "color" => "default"]],
+            [["action" => [
+                "type" => "text",
+                "payload" => '{"button": "thu"}',
+                "label" => "Четверг"],
+                "color" => "default"]],
+            [["action" => [
+                "type" => "text",
+                "payload" => '{"button": "fri"}',
+                "label" => "Пятница"],
+                "color" => "default"]],
+            [["action" => [
+                "type" => "text",
+                "payload" => '{"button": "sat"}',
+                "label" => "Суббота"],
+                "color" => "default"]],
+            [["action" => [
+                "type" => "text",
+                "payload" => '{"button": "sun"}',
+                "label" => "Воскресенье"],
+                "color" => "default"]]
+        ]
+    ];
+    return $keyboard;
 }
 
 
 function keyboard_choose_subject()
 {
-//    $keyboard = [
-//        "one_time" => false,
-//        "buttons" => [
-//            [["action" => [
-//                "type" => "text",
-//                "payload" => '{"button": "ms"}',
-//                "label" => "Математическая статистика"],
-//                "color" => "default"]],
-//            [["action" => [
-//                "type" => "text",
-//                "payload" => '{"button": "mpp"}',
-//                "label" => "Параллельное программирование"],
-//                "color" => "default"]],
-//            [["action" => [
-//                "type" => "text",
-//                "payload" => '{"button": "da"}',
-//                "label" => "Анализ данных"],
-//                "color" => "default"]],
-//            [["action" => [
-//                "type" => "text",
-//                "payload" => '{"button": "mt"}',
-//                "label" => "Методы трансляции"],
-//                "color" => "default"]],
-//            [["action" => [
-//                "type" => "text",
-//                "payload" => '{"button": "en"}',
-//                "label" => "Английский язык"],
-//                "color" => "default"]],
-//            [["action" => [
-//                "type" => "text",
-//                "payload" => '{"button": "fp"}',
-//                "label" => "Функциональное программирование"],
-//                "color" => "default"]]
-//        ]
-//    ];
-//    return $keyboard;
+    $keyboard = [
+        "one_time" => false,
+        "buttons" => [
+            [["action" => [
+                "type" => "text",
+                "payload" => '{"button": "ms"}',
+                "label" => "Математическая статистика"],
+                "color" => "default"]],
+            [["action" => [
+                "type" => "text",
+                "payload" => '{"button": "mpp"}',
+                "label" => "Параллельное программирование"],
+                "color" => "default"]],
+            [["action" => [
+                "type" => "text",
+                "payload" => '{"button": "da"}',
+                "label" => "Анализ данных"],
+                "color" => "default"]],
+            [["action" => [
+                "type" => "text",
+                "payload" => '{"button": "mt"}',
+                "label" => "Методы трансляции"],
+                "color" => "default"]],
+            [["action" => [
+                "type" => "text",
+                "payload" => '{"button": "en"}',
+                "label" => "Английский язык"],
+                "color" => "default"]],
+            [["action" => [
+                "type" => "text",
+                "payload" => '{"button": "fp"}',
+                "label" => "Функциональное программирование"],
+                "color" => "default"]]
+        ]
+    ];
+    return $keyboard;
 }
