cac73cac8c1ab28134a911b4ee335e9309762252
Sheveleva Nadegda <sheveleva-n02@mail.ru>
2022-12-01 01:26:39 +0300

    Change conf token

--- util/config.php
+++ util/config.php
@@ -6,6 +6,6 @@ define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
 
 define('VK_API_VERSION', '5.131'); //Используемая версия API
 define('VK_API_ENDPOINT', 'https://api.vk.com/method/');
-define('CALLBACK_API_CONFIRMATION_TOKEN', 'dfce9f4e'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('CALLBACK_API_CONFIRMATION_TOKEN', '48957d46'); //Строка для подтверждения адреса сервера из настроек Callback API
 define('VK_API_ACCESS_TOKEN', 'vk1.a.9j7qT1t6Q0Ck1sd74dyc38cboQacFV8WOIwlfC8PO-nzxcPZ6-8KArJ8P6A5Vfy5VVT_FzgVvr7ZQpovh2GvFdTMyDgtQ4W_XqI0Vdpg7LUdgw0WBg3fSSuBQq8SA5w22MNRMFZEt9YwsHelV0NLa_6RujYLHZeImP9fP8sPJZrrcZUy_MvPjoFRTEmWLuKi-xZbsW9AvROhiiOyEzLM2w'); //Ключ доступа сообщества
 
