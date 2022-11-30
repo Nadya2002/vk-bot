92d13cbfc6aa28bb033cdee9659a945dd5e90c08
Sheveleva Nadegda <sheveleva-n02@mail.ru>
2022-11-30 22:02:10 +0300

    Debug

--- api/bot.php
+++ api/bot.php
@@ -4,11 +4,11 @@ function bot_sendMessage($user_id, $text)
 {
     $users_get_response = vkApi_usersGet($user_id);
     $user = $users_get_response->response[0];
-    if ($text == 'Выбрать группу') {
+    if ($text == "Выбрать группу") {
         $keyboard = keyboard_choose_group();
     } else {
         $keyboard = create_keyboard();
     }
-    $msg = "Привет, {$user->first_name}!";
+    $msg = "Привет, {$user->first_name}!" . $text;
     vkApi_messagesSend($user_id, $msg, $keyboard);
 }
