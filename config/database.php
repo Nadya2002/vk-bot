1fdb0acd7aa5885a38253ff9934df3ff07094825
Sheveleva Nadegda <sheveleva-n02@mail.ru>
2022-12-04 21:33:16 +0300

    Change structure project

--- config/database.php
+++ config/database.php
@@ -6,14 +6,11 @@ class Database {
     private $password = "admin";
     public $conn;
     public function getConnection(){
-        log_msg("in get conn");
         $this->conn = null;
         try{
-            log_msg("in try");
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database_name, $this->username, $this->password);
             $this->conn->exec("set names utf8");
         }catch(PDOException $exception){
-            log_msg("Database could not be connected: " . $exception->getMessage());
             echo "Database could not be connected: " . $exception->getMessage();
         }
         return $this->conn;
